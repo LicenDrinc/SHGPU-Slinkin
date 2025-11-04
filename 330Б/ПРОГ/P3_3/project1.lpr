bf8b1e04038b3dd1bb108f0814b689160f9fb7eb
LicenDrinc <mihaoo4556@gmail.com>
2025-11-04 18:26:55 +0500

    13

--- 330Б/ПРОГ/P3_3/project1.lpr
+++ 330Б/ПРОГ/P3_3/project1.lpr
@@ -1,18 +1,28 @@
 program project1;
 
-uses Unit1;
+uses Unit1, Unit2, Classes;
 
 var human: TArrayHuman;
-
+    fl: TFileStream;
+    fl1: TArrayStream;
 begin
     human := TArrayHuman.Create;
+    fl := TFileStream.Create('3.txt', fmOpenReadWrite);
+    fl1 := TArrayStream.Create;
+    human.loadFromStream(fl1);
+    human.loadWithReader(fl);
 
     human.manualInputHuman;
     human.manualInputHuman;
 
     human.writeArrHuman('-----');
 
+    human.saveToStream(fl1);
+    human.saveWithWriter(fl);
+
     human.free;
+    fl.free;
+    fl1.free;
     readln;
 end.
 
--- 330Б/ПРОГ/P3_3/unit1.pas
+++ 330Б/ПРОГ/P3_3/unit1.pas
@@ -13,17 +13,23 @@ type
         child: array of ansistring;
     end;
 
-    TArrayHuman = class(TStream)
+    TArrayHuman = class
         public
             arrHuman: array of THuman;
 
-            constructor Create;
+            constructor Create();
             destructor Destroy; override;
 
             procedure manualInputHuman;
             procedure addHuman(fN, lN, pn, g, d, id: ansistring; c: array of ansistring);
             procedure writeHuman(H: THuman);
             procedure writeArrHuman(str: string = '');
+
+            procedure saveToStream(Stream: TStream);
+            procedure loadFromStream(Stream: TStream);
+
+            procedure saveWithWriter(Stream: TStream);
+            procedure loadWithReader(Stream: TStream);
     end;
 
 implementation
@@ -45,11 +51,11 @@ begin
     writeln('пол: ', H.gender ,'; д.р.: ', H.date);
     writeln('id: ', H.id);
     if (length(H.child) = 0) then exit;
-    for i:=0 to length(H.child) - 1 do
+    for i := 0 to length(H.child) - 1 do
     begin
         k := -1;
         System.write(i + 1,' ребёнка: ', H.child[i]);
-        for j:=0 to length(arrHuman) - 1 do
+        for j := 0 to length(arrHuman) - 1 do
             if (H.child[i] = arrHuman[j].id) then
                 k := j;
         if (k <> -1) then
@@ -61,7 +67,7 @@ procedure TArrayHuman.writeArrHuman(str: string);
 var i: integer;
 begin
     if (str <> '') then writeln(str);
-    for i:=0 to length(arrHuman) - 1 do
+    for i := 0 to length(arrHuman) - 1 do
     begin
         writeHuman(arrHuman[i]); writeln;
     end;
@@ -81,7 +87,7 @@ begin
     arrHuman[i].id := id;
     setLength(arrHuman[i].child, length(c));
     if (length(arrHuman[i].child) = 0) then exit;
-    for j:=0 to length(arrHuman[i].child) - 1 do
+    for j := 0 to length(arrHuman[i].child) - 1 do
         arrHuman[i].child[j] := copy(c[j], 0, length(c[j]));
 end;
 
@@ -90,26 +96,122 @@ var name, t, fN, lN, pn, g, d, id: ansistring;
     c: array of ansistring;
     i: integer;
 begin
-    System.write('ФИО: '); readln(name);
+    write('ФИО: '); readln(name);
     fN := copy(name, 0, pos(' ', name) - 1);
     t := copy(name, pos(' ', name) + 1, length(name) - pos(' ', name));
     lN := copy(t, 0, pos(' ', t) - 1);
     pn := copy(t, pos(' ', t) + 1, length(t) - pos(' ', name));
-    System.write('пол: '); readln(g);
-    System.write('д.р.: '); readln(d);
-    System.write('id: '); readln(id);
+    write('пол: '); readln(g);
+    write('д.р.: '); readln(d);
+    write('id: '); readln(id);
     i := 0;
     while (t <> '') or (i = 0) do
     begin
         setLength(c, i);
         if (i <> 0) then c[i - 1] := copy(t, 0, length(t));
-        System.write('id ', i + 1, ' ребёнка: '); readln(t);
+        write('id ', i + 1, ' ребёнка: '); readln(t);
         i := i + 1;
     end;
     addHuman(fN, lN, pn, g, d, id, c);
 end;
 
+procedure TArrayHuman.saveToStream(Stream: TStream);
+var i, j, count: Integer;
+begin
+    Stream.Size := 0;
+    Stream.Seek(0, soBeginning);
+    count := Length(arrHuman);
+    Stream.WriteBuffer(count, SizeOf(count));
 
+    for i := 0 to count - 1 do
+    begin
+        Stream.WriteAnsiString(arrHuman[i].firstName);
+        Stream.WriteAnsiString(arrHuman[i].lastName);
+        Stream.WriteAnsiString(arrHuman[i].patromynic);
+        Stream.WriteAnsiString(arrHuman[i].gender);
+        Stream.WriteAnsiString(arrHuman[i].date);
+        Stream.WriteAnsiString(arrHuman[i].id);
+
+        count := Length(arrHuman[i].child);
+        Stream.WriteBuffer(count, SizeOf(count));
+        for j := 0 to count - 1 do
+            Stream.WriteAnsiString(arrHuman[i].child[j]);
+    end;
+end;
+procedure TArrayHuman.loadFromStream(Stream: TStream);
+var i, j, count, childCount: Integer;
+begin
+    if (Stream.Size = 0) then exit;
+    Stream.ReadBuffer(count, SizeOf(count));
+    SetLength(arrHuman, count);
+
+    for i := 0 to count - 1 do
+    begin
+        arrHuman[i].firstName   := Stream.ReadAnsiString;
+        arrHuman[i].lastName    := Stream.ReadAnsiString;
+        arrHuman[i].patromynic  := Stream.ReadAnsiString;
+        arrHuman[i].gender      := Stream.ReadAnsiString;
+        arrHuman[i].date        := Stream.ReadAnsiString;
+        arrHuman[i].id          := Stream.ReadAnsiString;
+
+        Stream.ReadBuffer(childCount, SizeOf(childCount));
+        SetLength(arrHuman[i].child, childCount);
+        for j := 0 to childCount - 1 do
+            arrHuman[i].child[j] := Stream.ReadAnsiString;
+    end;
+end;
+
+procedure TArrayHuman.saveWithWriter(Stream: TStream);
+var Writer: TWriter;
+    i, j: Integer;
+begin
+    Writer := TWriter.Create(Stream, 4096);
+    try
+        Writer.WriteInteger(Length(arrHuman));
+        for i := 0 to Length(arrHuman) - 1 do
+        begin
+            Writer.WriteString(arrHuman[i].firstName);
+            Writer.WriteString(arrHuman[i].lastName);
+            Writer.WriteString(arrHuman[i].patromynic);
+            Writer.WriteString(arrHuman[i].gender);
+            Writer.WriteString(arrHuman[i].date);
+            Writer.WriteString(arrHuman[i].id);
+
+            Writer.WriteInteger(Length(arrHuman[i].child));
+            for j := 0 to Length(arrHuman[i].child) - 1 do
+                Writer.WriteString(arrHuman[i].child[j]);
+        end;
+    finally
+        Writer.free;
+    end;
+end;
+procedure TArrayHuman.loadWithReader(Stream: TStream);
+var Reader: TReader;
+    i, j, count, childCount: Integer;
+begin
+    Reader := TReader.Create(Stream, 4096);
+    try
+        count := Reader.ReadInteger;
+        SetLength(arrHuman, count);
+
+        for i := 0 to count - 1 do
+        begin
+            arrHuman[i].firstName  := Reader.ReadString;
+            arrHuman[i].lastName   := Reader.ReadString;
+            arrHuman[i].patromynic := Reader.ReadString;
+            arrHuman[i].gender     := Reader.ReadString;
+            arrHuman[i].date       := Reader.ReadString;
+            arrHuman[i].id         := Reader.ReadString;
+
+            childCount := Reader.ReadInteger;
+            SetLength(arrHuman[i].child, childCount);
+            for j := 0 to childCount - 1 do
+                arrHuman[i].child[j] := Reader.ReadString;
+        end;
+    finally
+        Reader.free;
+    end;
+end;
 
 end.
 
