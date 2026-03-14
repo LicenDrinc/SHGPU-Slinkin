82c925312fd4d00fac981199a57f1cd6519da0b8
LicenDrinc <mihaoo4556@gmail.com>
2026-03-14 09:54:08 +0500

    3_2_P3_6

---
+++ 330Б/ПРОГ/P3_6/backup/unit3.pas
@@ -0,0 +1,55 @@
+unit Unit3;
+
+{$mode ObjFPC}{$H+}
+
+interface
+
+uses
+    Classes, SysUtils, Forms, Controls, Graphics, Dialogs, IpHtml;
+
+type
+
+    { TForm3 }
+
+    TForm3 = class(TForm)
+        IpHtmlPanel1: TIpHtmlPanel;
+        procedure FormCreate(Sender: TObject);
+        procedure FormResize(Sender: TObject);
+    private
+    public
+    end;
+
+var Form3: TForm3;
+
+implementation
+
+uses Unit1;
+
+{$R *.lfm}
+
+procedure TForm3.FormCreate(Sender: TObject);
+begin
+    IpHtmlPanel1.SetHtmlFromStr(
+        '<html><head><title>Масштабирование</title></head>' +
+        '<body><h1>Масштабирование</h1>' +
+        '<p>Масштабирование используется для измененения масштаба графика в диапазоне [0.01,'+FloatToStr(Form1.maxLine)+'].</p>' +
+        '<p>Масштабирование производится отдельно для осей абсцисс и ординат.</p>' +
+        '<p>Изменение масштаба автоматически приводит к перерисовке графика.</p>' +
+        '</body></html>'
+    );
+    Top := 100; Left := 100;
+    Width := 500; Height := 500;
+    IpHtmlPanel1.Top := 0;
+    IpHtmlPanel1.Left := 0;
+    IpHtmlPanel1.Width := Width;
+    IpHtmlPanel1.Height := Height;
+end;
+
+procedure TForm3.FormResize(Sender: TObject);
+begin
+    IpHtmlPanel1.Width := Width;
+    IpHtmlPanel1.Height := Height;
+end;
+
+end.
+
--- 330Б/ПРОГ/P3_6/project1.lpr
+++ 330Б/ПРОГ/P3_6/project1.lpr
@@ -10,7 +10,7 @@ uses
     athreads,
     {$ENDIF}
     Interfaces, // this includes the LCL widgetset
-    Forms, pascalscript, Unit1
+    Forms, pascalscript, Unit1, Unit2, Unit3
     { you can add units after this };
 
 {$R *.res}
@@ -23,6 +23,8 @@ begin
     {$POP}
     Application.Initialize;
     Application.CreateForm(TForm1, Form1);
+    Application.CreateForm(TForm2, Form2);
+    Application.CreateForm(TForm3, Form3);
     Application.Run;
 end.
 
